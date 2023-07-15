ad47fc1025c8480c71fe881563ed79f42f42d65b
Phan Hai <haipmbdvn@gmail.com>
2023-07-15 12:04:31 +0700

    update controlller

--- API/Controllers/AsposeWordController.cs
+++ API/Controllers/AsposeWordController.cs
@@ -18,8 +18,51 @@ namespace API.Controllers
         [HttpGet("ConvertToPDF")]
         public async Task<IActionResult> ConvertToPDF()
         {
+            var result = _evi.ChuyenDoiSangPDF();
+            return Ok();
+        }
+
+        [HttpGet("TimKiemVaThayThe")]
+        public async Task<IActionResult> TimKiemVaThayThe()
+        {
+            var result = _evi.TimKiemVaThayThe();
+            return Ok();
+        }
+
+        [HttpGet("ThemHinhAnh")]
+        public async Task<IActionResult> ThemHinhAnh()
+        {
+            var result = _evi.ThemHinhAnh();
+            return Ok();
+        }
+
+        [HttpGet("TrichXuatHinhAnh")]
+        public async Task<IActionResult> TrichXuatHinhAnh()
+        {
+            var result = _evi.TrichXuatHinhAnh();
+            return Ok();
+        }
+
+        [HttpGet("ChenVaThaoTacBieuDo")]
+        public async Task<IActionResult> ChenVaThaoTacBieuDo()
+        {
             var result = _evi.ChenVaThaoTacBieuDo();
             return Ok();
         }
+
+        [HttpGet("BaoMat")]
+        public async Task<IActionResult> BaoMat()
+        {
+            var result = _evi.BaoMat();
+            return Ok();
+        }
+
+        [HttpGet("BaoMatVoiCHUKISO")]
+        public async Task<IActionResult> BaoMatVoiCHUKISO()
+        {
+            var result = _evi.BaoMatVoiCHUKISO();
+            return Ok();
+        }
+
     }
 }
